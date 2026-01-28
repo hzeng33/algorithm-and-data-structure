fa8807a96fb749159b9276af2d4d32278e54ee97
hzeng33 <hzeng9310@gmail.com>
2026-01-27 20:54:30 -0500

    Added sorting and recursion, split queue and stack in new folder

---
+++ Recursion/Fibonacci.java
@@ -0,0 +1,12 @@
+// Recursive implementation to calculate the n-th Fibonacci number
+public class Fibonacci { 
+
+    // Base case: n = 0 or 1
+    public static int fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        // Recursive case: fib(n) = fib(n - 1) + fib(n - 2)
+        return fibonacci(n-1) + fibonacci(n-2);
+    }
+}
---
+++ Recursion/fibonacci.cs
@@ -0,0 +1,14 @@
+﻿public class Fibonacci
+{
+    // Recursive implementation to calculate the n-th Fibonacci number
+    // Base case: n = 0 or 1
+    public static int FibonacciSeries(int n)
+    {
+        if (n <= 1)
+        {
+            return n;
+        }
+        // Recursive case: fib(n) = fib(n - 1) + fib(n - 2)
+        return FibonacciSeries(n - 1) + FibonacciSeries(n - 2);
+    }
+}
